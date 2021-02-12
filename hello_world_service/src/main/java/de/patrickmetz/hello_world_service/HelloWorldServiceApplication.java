df5f4f82ceaac457e247cc29ae262b3bebb43ce1
Patrick Metz <patrickmetz@web.de>
2021-02-12 22:40:54 +0100

    Hello World Service: initialer Commit

---
+++ hello_world_service/src/main/java/de/patrickmetz/hello_world_service/HelloWorldServiceApplication.java
@@ -0,0 +1,13 @@
+package de.patrickmetz.hello_world_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWorldServiceApplication.class, args);
+    }
+
+}
---
+++ hello_world_service/src/test/java/de/patrickmetz/hello_world_service/HelloWorldServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package de.patrickmetz.hello_world_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
