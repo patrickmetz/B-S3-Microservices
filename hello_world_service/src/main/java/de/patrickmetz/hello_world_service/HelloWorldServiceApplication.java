bb985f87d44fbd18b612aff23cd22c0f4a0ba8c0
Patrick Metz <patrickmetz@web.de>
2021-02-13 23:25:51 +0100

    Hello World Service: für Registry-Service konfiguriert

--- hello_world_service/src/main/java/de/patrickmetz/hello_world_service/HelloWorldServiceApplication.java
+++ hello_world_service/src/main/java/de/patrickmetz/hello_world_service/HelloWorldServiceApplication.java
@@ -2,8 +2,10 @@ package de.patrickmetz.hello_world_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class HelloWorldServiceApplication {
 
     public static void main(String[] args) {
