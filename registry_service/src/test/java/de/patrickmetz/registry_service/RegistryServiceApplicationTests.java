6cc274276744522428140d814bd2c7c4fb995a4b
Patrick Metz <patrickmetz@web.de>
2021-02-13 23:09:16 +0100

    Registry Service: Initialer Commit. Und bereits konfiguriert.

---
+++ registry_service/src/test/java/de/patrickmetz/registry_service/RegistryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package de.patrickmetz.registry_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
