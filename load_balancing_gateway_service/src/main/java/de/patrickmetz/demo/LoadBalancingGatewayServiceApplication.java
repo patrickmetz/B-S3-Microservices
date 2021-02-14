63b6c1101c50a89c1d8de2b387d9641d02f7632e
Patrick Metz <patrickmetz@web.de>
2021-02-14 23:27:06 +0100

    Load Balancing Gateway Service: implementiert

---
+++ load_balancing_gateway_service/src/main/java/de/patrickmetz/demo/LoadBalancingGatewayServiceApplication.java
@@ -0,0 +1,17 @@
+package de.patrickmetz.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+@EnableDiscoveryClient
+public class LoadBalancingGatewayServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoadBalancingGatewayServiceApplication.class, args);
+    }
+
+}
---
+++ load_balancing_gateway_service/src/test/java/de/patrickmetz/demo/LoadBalancingGatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package de.patrickmetz.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoadBalancingGatewayServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
