f971edd42049ee09322368d0e2a5b8a7b1f4c7b6
Patrick Metz <patrickmetz@web.de>
2021-02-14 23:26:18 +0100

    Hello World Service: Infoformat gefixt

--- hello_world_service/src/main/java/de/patrickmetz/hello_world_service/service/HelloWorldService.java
+++ hello_world_service/src/main/java/de/patrickmetz/hello_world_service/service/HelloWorldService.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class HelloWorldService implements HelloWorldServiceIF {
-    private static final String TEMPLATE = "Hello, %s!<br/>This is visit no. %d.<br/><br/>My id is %s.";
+    private static final String TEMPLATE = "Hello, %s!<br/>This is visit no. %d.<br/><br/>My id is: %s.";
 
     @Value("${eureka.instance.instance-id}")
     private String instanceId;
