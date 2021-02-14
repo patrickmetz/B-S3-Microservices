502b163ee8539dee9a20e79ea5fc54f94b2e0b2e
Patrick Metz <patrickmetz@web.de>
2021-02-14 20:35:29 +0100

    Hello World Service: Instanz-Id bei "GET /hello" ausgeben

--- hello_world_service/src/main/java/de/patrickmetz/hello_world_service/service/HelloWorldService.java
+++ hello_world_service/src/main/java/de/patrickmetz/hello_world_service/service/HelloWorldService.java
@@ -1,16 +1,19 @@
 package de.patrickmetz.hello_world_service.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class HelloWorldService implements HelloWorldServiceIF {
-    public static final String TEMPLATE = "Hello, %s! This is visit no. %d.";
+    private static final String TEMPLATE = "Hello, %s!<br/>This is visit no. %d.<br/><br/>My id is %s.";
 
-    //todo: replace with service id
+    @Value("${eureka.instance.instance-id}")
+    private String instanceId;
+
     private static int counter = 0;
 
     @Override
     public String greeting(String greeting) {
-        return String.format(TEMPLATE, greeting, ++counter);
+        return String.format(TEMPLATE, greeting, ++counter, instanceId);
     }
 }
