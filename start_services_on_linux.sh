4c00d3187d91da62f1b2c2366d51192dfc1aead2
Patrick Metz <patrickmetz@web.de>
2021-02-16 01:02:46 +0100

    Batch-/Shell-Skript-Nachrichten verbessert.

--- start_services_on_linux.sh
+++ start_services_on_linux.sh
@@ -1,21 +1,21 @@
 echo "1/4: Starting 'Registry Service' in background."
 bash -c "exec -a RegistryService java -jar ./registry_service/build/libs/registry_service-0.0.1-SNAPSHOT.jar &>/dev/null &"
-echo "Waiting 10 seconds."
-sleep 10
+echo "Waiting 15 seconds."
+sleep 15
 
 echo "2/4: Starting '1st Hello World Service' in background."
 bash -c "exec -a 1stHelloWorldService java -jar ./hello_world_service/build/libs/hello_world_service-0.0.1-SNAPSHOT.jar &>/dev/null &"
-echo "Waiting 10 seconds."
-sleep 10
+echo "Waiting 15 seconds."
+sleep 15
 
 echo "3/4: Starting '2nd Hello World Service' in background."
 bash -c "exec -a 2ndHelloWorldService java -jar ./hello_world_service/build/libs/hello_world_service-0.0.1-SNAPSHOT.jar &>/dev/null &"
-echo "Waiting 10 seconds."
-sleep 10
+echo "Waiting 15 seconds."
+sleep 15
 
 echo "4/4: Starting 'Load Balancing Gateway Service' in background."
 bash -c "exec -a LoadBalancingGatewayService java -jar ./load_balancing_gateway_service/build/libs/load_balancing_gateway_service-0.0.1-SNAPSHOT.jar &>/dev/null &"
-echo "Waiting 10 seconds."
-sleep 10
+echo "Waiting 15 seconds."
+sleep 15
 
 echo "Done."
--- start_services_on_windows.bat
+++ start_services_on_windows.bat
@@ -1,16 +1,22 @@
 @echo off 
 
-start "Registry Service" java -jar .\registry_service\build\libs\registry_service-0.0.1-SNAPSHOT.jar
+echo 1/4: Starting 'Registry Service' in background.
+start /min "Registry Service" java -jar .\registry_service\build\libs\registry_service-0.0.1-SNAPSHOT.jar
+echo Waiting 15 seconds.
+REM windows equivalent of sleeping 15 seconds
+ping 127.0.0.1 -n 18 > nul
 
-REM windows equivalent of sleeping 10 seconds
-ping 127.0.0.1 -n 12 > nul
+echo 2/4: Starting '1st Hello World Service' in background.
+start /min "'Hello World'-Service 1/2" java -jar .\hello_world_service\build\libs\hello_world_service-0.0.1-SNAPSHOT.jar
+echo Waiting 15 seconds.
+ping 127.0.0.1 -n 18 > nul
 
-start "'Hello World'-Service 1/2" java -jar .\hello_world_service\build\libs\hello_world_service-0.0.1-SNAPSHOT.jar
+echo 3/4: Starting '2nd Hello World Service' in background.
+start /min "'Hello World'-Service 2/2"  java -jar .\hello_world_service\build\libs\hello_world_service-0.0.1-SNAPSHOT.jar
+echo Waiting 15 seconds.
+ping 127.0.0.1 -n 18 > nul
 
-ping 127.0.0.1 -n 12 > nul
-
-start "'Hello World'-Service 2/2"  java -jar .\hello_world_service\build\libs\hello_world_service-0.0.1-SNAPSHOT.jar
-
-ping 127.0.0.1 -n 12 > nul
-
-start "Load Balancing Gateway Service"  java -jar .\load_balancing_gateway_service\build\libs\load_balancing_gateway_service-0.0.1-SNAPSHOT.jar
+echo 4/4: Starting 'Load Balancing Gateway Service' in background.
+start /min "Load Balancing Gateway Service"  java -jar .\load_balancing_gateway_service\build\libs\load_balancing_gateway_service-0.0.1-SNAPSHOT.jar
+echo Waiting 15 seconds.
+ping 127.0.0.1 -n 18 > nul
