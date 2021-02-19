4d68ee00c8c2d2b0b1c74984ee2775645451aeaf
Patrick Metz <patrickmetz@web.de>
2021-02-19 20:39:09 +0100

    Skript-/Batch-Dateien: Umbenannt und Lesbarkeit der Ausgaben verbessert.

--- start_windows_services.bat
+++ start_windows_services.bat
@@ -1,22 +1,30 @@
 @echo off 
 
+cls
+
 echo 1/4: Starting 'Registry Service' in background.
 start /min "Registry Service" java -jar .\registry_service\build\libs\registry_service-0.0.1-SNAPSHOT.jar
 echo Waiting 15 seconds.
+echo:
 REM windows equivalent of sleeping 15 seconds
 ping 127.0.0.1 -n 18 > nul
 
 echo 2/4: Starting '1st Hello World Service' in background.
 start /min "'Hello World'-Service 1/2" java -jar .\hello_world_service\build\libs\hello_world_service-0.0.1-SNAPSHOT.jar
 echo Waiting 15 seconds.
+echo:
 ping 127.0.0.1 -n 18 > nul
 
 echo 3/4: Starting '2nd Hello World Service' in background.
 start /min "'Hello World'-Service 2/2"  java -jar .\hello_world_service\build\libs\hello_world_service-0.0.1-SNAPSHOT.jar
 echo Waiting 15 seconds.
+echo:
 ping 127.0.0.1 -n 18 > nul
 
 echo 4/4: Starting 'Load Balancing Gateway Service' in background.
 start /min "Load Balancing Gateway Service"  java -jar .\load_balancing_gateway_service\build\libs\load_balancing_gateway_service-0.0.1-SNAPSHOT.jar
 echo Waiting 15 seconds.
+echo:
 ping 127.0.0.1 -n 18 > nul
+
+echo Done.
--- stop_linux_services.sh
+++ stop_linux_services.sh
@@ -1,13 +1,19 @@
+clear
+
 echo "1/4: Stopping 'Load Balancing Gateway Service' in background."
 pkill --signal 15 -f LoadBalancingGatewayService
+sleep 1
 
 echo "2/4: Stopping '2nd Hello World Service' in background."
 pkill --signal 15 -f 2ndHelloWorldService
+sleep 1
 
 echo "3/4: Stopping '1st Hello World Service' in background."
 pkill --signal 15 -f 1stHelloWorldService
+sleep 1
 
 echo "4/4: Stopping 'Registry Service' in background."
 pkill --signal 15 -f RegistryService
+sleep 1
 
 echo "Done."
