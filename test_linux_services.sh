a3bcd1b7c24b142cf1bd579b996be367dd8d7c81
Patrick Metz <patrickmetz@web.de>
2021-02-22 22:52:07 +0100

    Test-Shell-Skript für Linux-Services verbessert

--- test_linux_services.sh
+++ test_linux_services.sh
@@ -1,32 +1,42 @@
 clear
 
-echo "1st call to a 'Hello World Service'"
+echo "1/8: 1st call to a 'Hello World Service'"
 curl localhost:8080/hello
 echo -e '\n'
 sleep 1
 
-echo "2nd call to a 'Hello World Service'"
+echo "2/8: 2nd call to a 'Hello World Service'"
 curl localhost:8080/hello
 echo -e '\n'
 sleep 1
 
-echo "3rd call to a 'Hello World Service'"
+echo "3/8: 3rd call to a 'Hello World Service'"
 curl localhost:8080/hello
 echo -e '\n'
 sleep 1
 
-echo "4th call to a 'Hello World Service'"
+echo "4/8: 4th call to a 'Hello World Service'"
 curl localhost:8080/hello
 echo -e '\n'
 sleep 1
 
-echo "1st shutdown of a 'Hello World Service'"
+echo "5/8: 1st shutdown of a 'Hello World Service'"
 curl -X POST localhost:8080/actuator/shutdown
 echo -e '\n'
 sleep 1
 
-echo "2nd shutdown of a 'Hello World Service'"
+echo "6/8: 2nd shutdown of a 'Hello World Service'"
 curl -X POST localhost:8080/actuator/shutdown
+echo -e '\n'
+sleep 1
+
+echo "7/8: 5th call to a 'Hello World Service'"
+curl localhost:8080/hello
+echo -e '\n'
+sleep 1
+
+echo "8/8: 6th call to a 'Hello World Service'"
+curl localhost:8080/hello
 echo -e '\n'
 sleep 1
 
